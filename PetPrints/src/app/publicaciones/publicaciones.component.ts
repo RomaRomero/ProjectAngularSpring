3a35f3b2d9082b849dde9d8c82e1b27b7f7f60af
Bryam Romero <72166434+RomaRomero@users.noreply.github.com>
2020-10-18 18:54:19 -0500

    Add files via upload

--- PetPrints/src/app/publicaciones/publicaciones.component.ts
+++ PetPrints/src/app/publicaciones/publicaciones.component.ts
@@ -1,1 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Publicacion, PublicacionesService } from '../servicios/publicaciones.service';
 
+@Component({
+  selector: 'app-publicaciones',
+  templateUrl: './publicaciones.component.html',
+  styleUrls: ['./publicaciones.component.css']
+})
+export class PublicacionesComponent implements OnInit {
+
+  listadoPublicaciones:Publicacion[]=[];
+
+  constructor(private publicacionesService:PublicacionesService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.listadoPublicaciones=this.publicacionesService.getPublicaciones();
+  }
+
+}
