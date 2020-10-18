b025a129e7cb535dae449651bd5abf5bbe6fa04e
Bryam Romero <72166434+RomaRomero@users.noreply.github.com>
2020-10-18 18:42:34 -0500

    Create menup.component.ts

---
+++ PetPrints/src/app/menup/menup.component.ts
@@ -0,0 +1,107 @@
+<div class="container" style="margin-top:30px">
+    <div class="row">
+        <div class="col-sm-4">
+            <h2>Bienvenido</h2>
+            <h5>Proyecto desarroollado en :</h5>
+            <div class="fakeimg">
+                <div class="container">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfN7kb6RzQ8jWU70snEF73sCdye8DE5dGjOQ&usqp=CAU" alt="">
+                </div>
+            </div>
+
+            <p align="justified">Es un framework opensource desarrollado por Google para facilitar la creación y programación de aplicaciones web de una
+            sola página, las webs SPA (Single Page Application).</p>
+            <h3>Inicia Sesion en Pet Prints</h3>
+            <p>Selecciona una opcion.</p>
+            <ul class="nav nav-pills flex-column">
+                <li class="nav-item">
+                    <a class="btn btn-outline-info btn-block" routerLink="Crear" routerLinkActive="active">Crear Cuenta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-outline-danger btn-block" href="**">Iniciar Sesion con Google</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-outline-primary btn-block" href="**">Iniciar Sesion con Facebook</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-outline-success btn-block" routerLink="Login" routerLinkActive="active">Iniciar Sesion</a>
+                    <!-- <a class="nav-link disabled" href="#">Disabled</a> -->
+                </li>
+            </ul>
+            <hr class="d-sm-none">
+        </div>
+        <div class="col-sm-8">
+        <h2>Ayudanos a reunir a las mascotas con sus dueños</h2>
+        <h5>Logros alcanzados, Oct 4, 2020</h5>
+    
+            <div id="demo" class="carousel slide" data-ride="carousel">
+            
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                </ul>
+                <!-- The slideshow -->
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img src="https://estag.fimagenes.com/img/3/T/m/B/TmB_900.jpg" height="500" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://www.fundacion-affinity.org/sites/default/files/que-hacer-si-encuentras-a-un-perro-perdido-o-abandonado.jpg"  height="500" alt="Chicago">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://www.planv.com.ec/sites/default/files/perros-en-la-via-dsc_0002.jpg" height="500" alt="New York">
+                            </div>
+                        </div> 
+            
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#demo" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
+            
+            </div>
+            <br>
+            <div class="container">
+                <h3>Información acerca de desarrollores :</h3>
+                <p>Las personas involucradas en la creación de esta plataforma se consideran un equipo totalmente capacitado y apto para el desarrollo de diferentes tipos de actividades,  demostrando absoluto comnpromiso implementando mejoras y mucho más </p>
+            </div>
+            <div class="row-col">
+                <div class="card-deck">
+                    <!-- 4 of these -->
+                    <div class="card">
+                        <img class="card-img-top " src="https://scontent.fcue3-1.fna.fbcdn.net/v/t1.0-9/118615431_161164268949392_1835513899741728444_o.jpg?_nc_cat=102&_nc_sid=174925&_nc_ohc=0lZYQnNdQqIAX_t8KHp&_nc_ht=scontent.fcue3-1.fna&oh=567bbf6f44774ca72f9f2be13e6754fa&oe=5F9E0E33" >
+                        <div class="card-body">
+                            <h5>Christian Caguana</h5>
+                            <p>Desarrollador Front End con experiencia en Android ,React entre otros.</p>
+                        <a href="Acercade" class="btn btn-primary">Ver mas</a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top img-adjusted" src="https://scontent.fcue3-1.fna.fbcdn.net/v/t1.0-9/84982644_2835037136576776_8618493642027302912_n.jpg?_nc_cat=107&_nc_sid=174925&_nc_ohc=OKRuCBUPdGgAX-Bsvqq&_nc_ht=scontent.fcue3-1.fna&oh=d6975b80946b41e8d1cda4d4e40a90f5&oe=5F9FBB52">
+                        <div class="card-body">
+                        <h5>Geovanny Arevalo</h5>
+                        <p>Desarrollador Front End con experiencia en Android ,React, Angular , y otras areas</p>
+                <a href="Acercade" class="btn btn-primary">Ver Perfil</a>
+                    </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top img-adjusted" src="https://scontent.fcue3-1.fna.fbcdn.net/v/t1.0-9/118474267_2691880581053177_1161031711995442971_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=qu0dB3WLbEsAX-53PT4&_nc_ht=scontent.fcue3-1.fna&oh=5a6b7b22744ba4bab3a9566972de4981&oe=5F9FC58C" >
+                        <div class="card-body">
+                            <h5>Bryam Romero</h5>
+                            <p>Desarrollador Front End , Back End con experiencia en Android , Angular ,Spring , Netbeans , Python.</p>
+                        <a href="Acercade" class="btn btn-primary">Ver Perfil</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+</div>
+<br>
+<br>
